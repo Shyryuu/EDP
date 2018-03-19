47d16df58c9c04017d4782311f355792d8a72d7a
Shyryuu <ahernandezr1996@gmail.com>
2018-03-19 19:36:56 +0100

    Created HALO temptative datatypes.

--- include/EDP.h
+++ include/EDP.h
@@ -1,5 +1,6 @@
 
 #include "mpi.h"
+#include <vector>
 
 void crash();
 
@@ -11,3 +12,17 @@ int commsize(MPI_Comm);
 
 int rank();
 int rank(MPI_Comm);
+
+struct HALO_data_package
+{
+	double *start, *end;
+};
+
+struct HALO_update_unit
+{
+	unsigned int partner;
+	std::vector<HALO_data_package> spackage;
+	std::vector<HALO_data_package> rpackage;
+};
+
+typedef std::vector<HALO_update_unit> HALO_update_data;
